6b93767fe1f9fbf0ad805968f85193274e358e39
Antti Arrak <antti@asus.tuf.local>
2020-06-25 23:08:32 +0300

    Routeros test script

---
+++ RouterOs/script1.lua
@@ -0,0 +1,4 @@
+# This contains script that will be downloaded and updated
+# 1.1
+:log info "Scriptfile version 1.1"
+/system script add name=tmp.script policy=ftp source=":log info \"Test\"\r\n:log info \"Test 2\""
